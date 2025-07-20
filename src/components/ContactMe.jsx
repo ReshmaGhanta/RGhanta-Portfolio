0448d7abc436d2a1d474dec5f0531a46b2239e69
ReshmaGhanta <reshma@covertalert.ai>
2025-07-20 14:54:34 -0400

    Email sending code

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { cname } from "@/lib/utils";
 import { FaDiscord } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
-
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import { set } from 'immutable';
 
 
 const ContactMe = () => {
+  const [formInputs, setFormInputs] = useState({});
+  const [isSubmitting,setIsSubmitting]=useState(false);
+
+  const serviceId=import.meta.env.VITE_SERVICE_ID;
+  const templateId=import.meta.env.VITE_TEMPLATE_ID;
+  const publicKey=import.meta.env.VITE_PUBLIC_KEY;
+
+  const handleChange = (event) => {
+    setFormInputs(values => ({...values, [event.target.name]: event.target.value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setIsSubmitting(true);
+
+    
+
+    const templateParams={
+      fromName:formInputs.name,
+      fromEmail:formInputs.email,
+      toName:'Reshma Ghanta',
+      message:formInputs.message
+    }
+
+    // Send email using emailJS
+    emailjs
+    .send(serviceId, templateId, templateParams, publicKey).then((response) => {
+        console.log('SUCCESS!',response);
+        setFormInputs({})
+        setTimeout(()=>{
+          toast('Message Sent successfully!', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            })
+            setIsSubmitting(false);
+      },1500)
+        
+      })
+      .catch((error) => {
+        console.log('FAILED...', error);
+      });
+
+  }
 
   function openEmail(){
     window.open('mailto:reshma.ghanta@gmail.com');
@@ -26,22 +78,33 @@ const ContactMe = () => {
      
  <div className="flex justify-center">
            <div className="bg-card p-10 rounded-xl shadow-md  w-full max-w-2xl flex items-center justify-center">
-              <form className='space-y-6 w-full max-w-md'>
+              <form className='space-y-6 w-full max-w-md' onSubmit={handleSubmit}>
                     <div>
                           <label htmlFor='name'className='block text-md font-medium mb-3 mt-4'> Your Name</label>
-                          <input type="text" id="name" name="name" required className='w-full px-5 py-4 rounded-md text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary' placeholder='Enter your full name ...'/>
+                          <input type="text" id="name" name="name" required className='w-full px-5 py-4 rounded-md text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary' placeholder='Enter your full name ...' value={formInputs.name ||"" }  onChange={handleChange}/>
                     
                           <label htmlFor='email'className='block text-md font-medium mb-3 mt-4'> Your Email</label>
-                          <input type="email" id="email" name="email" required className='w-full px-5 py-4 rounded-md  text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary' placeholder='hello@gmail.com ...'/>
+                          <input type="email" id="email" name="email" required className='w-full px-5 py-4 rounded-md  text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary' placeholder='hello@gmail.com ...' value={formInputs.email || ""} onChange={handleChange}/>
                    
                           <label htmlFor='message'className='block text-md font-medium mb-3 mt-2'> Your Message</label>
-                          <textarea  id="message" name="message" required className='w-full mb-6 px-5 py-4 rounded-md text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none' placeholder='Hello!! I would like to talk about ...'/>
+                          <textarea  id="message" name="message" required className='w-full mb-6 px-5 py-4 rounded-md text-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none' placeholder='Hello!! I would like to talk about ...' value={formInputs.message || ""} onChange={handleChange}/>
                    
-                          <button type="submit" className={cname("cosmic-button w-full flex items-center justify-center gap-2")}>
-                            Send Message
-                            <FiSend size={16}/>
-                           {/* <Send size={16}></Send> */}
+                          <button type="submit" disabled={isSubmitting} className={cname("cosmic-button w-full flex items-center justify-center gap-2")}>
+                           {isSubmitting? "Sending" :"Send Message"} <FiSend size={16}/>
                            </button>
+                           <ToastContainer
+                                  position="bottom-center"
+                                  autoClose={5000}
+                                  hideProgressBar={false}
+                                  newestOnTop={false}
+                                  closeOnClick={false}
+                                  rtl={false}
+                                  pauseOnFocusLoss
+                                  draggable
+                                  pauseOnHover
+                                  theme="light"
+                                  
+                          />
                     </div>
               </form>
               </div>
